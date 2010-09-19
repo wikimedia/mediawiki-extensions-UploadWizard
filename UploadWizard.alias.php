edff80d7104b591fc7a94bc249cda4966642bca3
Siebrand Mazeland <siebrand@users.mediawiki.org>
2010-09-19 07:41:39 +0000

    Localisation updates for special page aliases for extensions from translatewiki.net

--- UploadWizard.alias.php
+++ UploadWizard.alias.php
@@ -4,10 +4,27 @@
  * The page title can be customized into another language
 */
 
-$aliases = array();
-
-/** English */
-$aliases['en'] = array(
+/** English (English) */
+$specialPageAliases['en'] = array(
 	'UploadWizard' => array( 'UploadWizard' ),
 );
 
+/** Italian (Italiano) */
+$specialPageAliases['it'] = array(
+	'UploadWizard' => array( 'CaricamentoGuidato' ),
+);
+
+/** Dutch (Nederlands) */
+$specialPageAliases['nl'] = array(
+	'UploadWizard' => array( 'WizardUploaden' ),
+);
+
+/** Sinhala (සිංහල) */
+$specialPageAliases['si'] = array(
+	'UploadWizard' => array( 'උඩුගත_කිරීමේ_මාය_අඳුන' ),
+);
+
+/**
+ * For backwards compatibility with MediaWiki 1.15 and earlier.
+ */
+$aliases =& $specialPageAliases;
