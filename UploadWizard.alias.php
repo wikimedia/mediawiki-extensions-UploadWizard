d22a6bae5d877fa05a491febe57bb68092865391
Siebrand Mazeland <siebrand@users.mediawiki.org>
2010-09-19 09:01:11 +0000

    Revert r73306. Causes many notices.

--- UploadWizard.alias.php
+++ UploadWizard.alias.php
@@ -4,8 +4,6 @@
  * The page title can be customized into another language
 */
 
-$specialPageAliases = array();
-
 /** English (English) */
 $specialPageAliases['en'] = array(
 	'UploadWizard' => array( 'UploadWizard' ),
