71f61c41c3941915ef2f11595eff37339d1d80ac
Siebrand Mazeland <siebrand@users.mediawiki.org>
2010-10-31 17:55:47 +0000

    Localisation updates from translatewiki.net

--- UploadWizard.alias.php
+++ UploadWizard.alias.php
@@ -11,9 +11,29 @@ $specialPageAliases['en'] = array(
 	'UploadWizard' => array( 'UploadWizard' ),
 );
 
+/** German (Deutsch) */
+$specialPageAliases['de'] = array(
+	'UploadWizard' => array( 'Hochlade-Assistent' ),
+);
+
+/** Interlingua (Interlingua) */
+$specialPageAliases['ia'] = array(
+	'UploadWizard' => array( 'Assistente_de_incargamento' ),
+);
+
 /** Italian (Italiano) */
 $specialPageAliases['it'] = array(
 	'UploadWizard' => array( 'CaricamentoGuidato' ),
+);
+
+/** Japanese (日本語) */
+$specialPageAliases['ja'] = array(
+	'UploadWizard' => array( 'アップロードウィザード' ),
+);
+
+/** Malayalam (മലയാളം) */
+$specialPageAliases['ml'] = array(
+	'UploadWizard' => array( 'അപ്‌ലോഡ്_സഹായി' ),
 );
 
 /** Dutch (Nederlands) */
