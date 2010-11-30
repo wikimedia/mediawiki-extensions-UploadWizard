2a0c4ac6dcfd30a46a5fd9d91a9bb0cefc959faa
Roan Kattouw <catrope@users.mediawiki.org>
2010-11-30 11:03:28 +0000

    Fix r77466 for nonexistent messages

--- resources/language/mw.Language.js
+++ resources/language/mw.Language.js
@@ -52,10 +52,10 @@ var JQUERY_SWAP_STRING = 'ZjQuerySwapZ';
 	mw.getMsg = function( messageKey , args ) {		
 
 		// Check for missing message key
-		if ( ! messageCache[ messageKey ] && 'msg' in mediaWiki ) {
+		if ( ! messageCache[ messageKey ] ) {
 			// Try the ResourceLoader's message store
 			// FIXME: The two message stores should be unified in the future
-			var rlMsg = mediaWiki.msg( messageKey );
+			var rlMsg = 'msg' in mediaWiki ? mediaWiki.msg( messageKey ) : ( '<' + messageKey + '>' );
 			if ( rlMsg == ( '<' + messageKey + '>' ) ) {
 				return '[' + messageKey + ']';
 			}
