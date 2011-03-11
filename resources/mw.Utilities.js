f135cf824ccec0bdbddceae56311283a5036ed29
Neil Kandalgaonkar <neilk@users.mediawiki.org>
2011-03-11 01:46:04 +0000

    fixed docs

--- resources/mw.Utilities.js
+++ resources/mw.Utilities.js
@@ -3,30 +3,29 @@
 ( function( mw ) {
 
 	/**
-	* Check if a value is null, undefined, or the empty string. 
-	*
-	* @param {Object} object Object to be checked
-	* @return {Boolean}
-	*/
+	 * Check if a value is null, undefined, or the empty string. 
+	 *
+	 * @param {object} object Object to be checked
+	 * @return {boolean}
+	 */
 	mw.isEmpty = function( o ) {
 		return ! mw.isDefined( o ) || o === null || ( typeof o === 'string' && o === '' ); 
 	};
 
 	/**
 	 * Check if something is defined
-	 * (inlineable?)
-	 * @param {Object}
-	 * @return boolean
+	 * @param {object}
+	 * @return {boolean}
 	 */
-	mw.isDefined = function( obj ) {
-		return typeof obj !== 'undefined'; 
+	mw.isDefined = function( o ) {
+		return typeof o !== 'undefined'; 
 	};
 
 
 	/**
 	 * Upper-case the first letter of a string.
-	 * @param string
-	 * @return string with first letter uppercased.
+	 * @param {string}
+	 * @return {string} with first letter uppercased.
 	 */
 	mw.ucfirst = function( s ) {
 		return s.substring(0,1).toUpperCase() + s.substr(1);
