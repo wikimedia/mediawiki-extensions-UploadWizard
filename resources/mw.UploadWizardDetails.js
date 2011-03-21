cfeedc764b3ab023be6bf7dacee943a2f9cd576d
Ryan Kaldari <kaldari@users.mediawiki.org>
2011-03-21 05:30:59 +0000

    switching title field from textarea to text input

--- resources/mw.UploadWizardDetails.js
+++ resources/mw.UploadWizardDetails.js
@@ -45,7 +45,7 @@ mw.UploadWizardDetails = function( upload, containerDiv ) {
 	//    http://commons.wikimedia.org/wiki/MediaWiki:Filename-prefix-blacklist
 	//    XXX make sure they can't use ctrl characters or returns or any other bad stuff.
 	_this.titleId = "title" + _this.upload.index;
-	_this.titleInput = $j( '<textarea type="text" id="' + _this.titleId + '" name="' + _this.titleId + '" rows="1" class="mwe-title mwe-long-textarea"></textarea>' )
+	_this.titleInput = $j( '<input type="text" id="' + _this.titleId + '" name="' + _this.titleId + '" class="mwe-title"/>' )
 		.keyup( function() { 
 			_this.upload.title.setNameText( _this.titleInput.value );
 			// TODO update a display of filename 
