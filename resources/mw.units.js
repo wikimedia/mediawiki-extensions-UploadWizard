a90ac90569f6b139b76f06bc05f7969c569e4354
Neil Kandalgaonkar <neilk@users.mediawiki.org>
2011-05-17 14:49:25 +0000

    better formatting for file sizes -- similar to / should replace routine in mw.special.upload.js

--- resources/mw.units.js
+++ resources/mw.units.js
@@ -1,35 +1,26 @@
-/**
- * Format a size in bytes for output, using an appropriate
- * unit (B, KB, MB or GB) according to the magnitude in question
- *
- * @param size Size to format
- * @return string Plain text (not HTML)
- */
-mw.units.bytes = function ( size ) {
-	// For small sizes no decimal places are necessary
-	var round = 0;
-	var msg = '';
-	if ( size > 1024 ) {
-		size = size / 1024;
-		if ( size > 1024 ) {
-			size = size / 1024;
-			// For MB and bigger two decimal places are smarter
-			round = 2;
-			if ( size > 1024 ) {
-				size = size / 1024;
-				msg = 'mwe-size-gigabytes';
-			} else {
-				msg = 'mwe-size-megabytes';
-			}
-		} else {
-			msg = 'mwe-size-kilobytes';
-		}
-	} else {
-		msg = 'mwe-size-bytes';
-	}
-	// JavaScript does not let you choose the precision when rounding
-	var p = Math.pow( 10, round );
-	size = Math.round( size * p ) / p;
-	return gM( msg , size );
-};
+( function( mw ) { 
 
+	var scales = [ 'bytes', 'kilobytes', 'megabytes', 'gigabytes', 'terabytes' ];
+
+	mw.units = {
+
+		/**
+		 * Format a size in bytes for output, using an appropriate
+		 * unit (B, KB, MB, GB, or TB) according to the magnitude in question
+		 *
+		 * @param {Number} size, positive integer
+		 * @return {String} formatted size
+		 */
+		bytes: function ( size ) {
+			var i = 0;
+			// while the scale is less than terabytes, bit-shift size over by 1024
+			while ( size >= 1024 && i < scales.length ) {
+				size /= 1024.0;
+				i++;
+			}
+			return gM( 'size-' + scales[i], size.toFixed( i > 1 ? 2 : 0 ) );
+		}
+	};
+
+} )( mediaWiki );
+	
