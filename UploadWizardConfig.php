b160f2346e1e768df313bedb02eafd200ce64dd8
Jeroen De Dauw <jeroendedauw@users.mediawiki.org>
2011-07-23 22:45:56 +0000

    fu r92970 - added docs, fixed var name typo and added check to only hold into account enabled campaigns

--- UploadWizardConfig.php
+++ UploadWizardConfig.php
@@ -13,6 +13,16 @@
  */
 class UploadWizardConfig {
 	
+	/**
+	 * Returns the globally configuration, optionaly combined with campaign sepcific
+	 * configuration. 
+	 * 
+	 * @since 1.2
+	 * 
+	 * @param string|null $campaignName
+	 * 
+	 * @return array
+	 */
 	public static function getConfig( $campaignName = null ) {
 		global $wgUploadWizardConfig;
 		static $mergedConfig = false;
@@ -28,21 +38,36 @@ class UploadWizardConfig {
 		return $wgUploadWizardConfig;
 	}
 	
+	/**
+	 * Returns the default global config, from UploadWizard.config.php.
+	 * 
+	 * @since 1.2
+	 * 
+	 * @return array
+	 */
 	protected static function getDefaultConfig() {
 		global $wgUpwizDir;
 		$configPath =  $wgUpwizDir . '/UploadWizard.config.php';
 		return is_file( $configPath ) ? include( $configPath ) : array();
 	}
 	
+	/**
+	 * Returns the configuration of the specified campaign, 
+	 * or an empty array when the campaign is not found or not enabled.
+	 * 
+	 * @since 1.2
+	 *
+	 * @param string $campaignName
+	 * 
+	 * @return array
+	 */
 	protected static function getCampaignConfig( $campaignName ) {
 		$capmaignSettings = array();
 		
-		if ( !is_null( $capaignName ) ) {
-			$capaign = UploadWizardCampaign::newFromName( $capaignName );
-			
-			if ( $capaign !== false ) {
-				return $capaign->getConfig();
-			}
+		$capaign = UploadWizardCampaign::newFromName( $campaignName );
+		
+		if ( $capaign !== false && $capaign->getIsEnabled() ) {
+			return $capaign->getConfig();
 		}
 		
 		return array();
