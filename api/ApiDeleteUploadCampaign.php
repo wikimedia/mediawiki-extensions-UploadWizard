e9640568dd1f088ed6455cb37e2e6bccae58d25f
Sam Reed <reedy@users.mediawiki.org>
2011-10-03 22:42:58 +0000

    Wrap text by trimming lines
    Trim trailing whitespace

--- api/ApiDeleteUploadCampaign.php
+++ api/ApiDeleteUploadCampaign.php
@@ -13,47 +13,47 @@
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
 class ApiDeleteUploadCampaign extends ApiBase {
-	
+
 	public function __construct( $main, $action ) {
 		parent::__construct( $main, $action );
 	}
-	
+
 	public function execute() {
 		global $wgUser;
-		
+
 		if ( !$wgUser->isAllowed( 'upwizcampaigns' ) || $wgUser->isBlocked() ) {
 			$this->dieUsageMsg( array( 'badaccess-groups' ) );
 		}
-		
+
 		$params = $this->extractRequestParams();
-		
+
 		$everythingOk = true;
-		
+
 		foreach ( $params['ids'] as $id ) {
 			$campaign = new UploadWizardCampaign( $id );
 			$everythingOk = $campaign->deleteFromDB() && $everythingOk;
 		}
-		
+
 		$this->getResult()->addValue(
 			null,
 			'success',
 			$everythingOk
 		);
 	}
-	
+
 	public function needsToken() {
 		return true;
 	}
-	
+
 	public function getTokenSalt() {
 		$params = $this->extractRequestParams();
 		return 'deletecampaign' . implode( '|', $params['ids'] );
 	}
-	
+
 	public function mustBePosted() {
 		return true;
 	}
-	
+
 	public function getAllowedParams() {
 		return array(
 			'ids' => array(
@@ -64,21 +64,22 @@ class ApiDeleteUploadCampaign extends ApiBase {
 			'token' => null,
 		);
 	}
-	
+
 	public function getParamDescription() {
 		return array(
 			'ids' => 'The IDs of the campaigns to delete',
 			'token' => 'Edit token. You can get one of these through prop=info.',
 		);
 	}
-	
+
 	public function getDescription() {
 		return array(
-			'API module for deleting Upload Campaigns, associated with UploadWizard. Do not rely on this, it is an API method ' 
-			. 'mostly for developer convenience. This does not mean an Upload Campaign editing API will ever be available.'
+			'API module for deleting Upload Campaigns, associated with UploadWizard.',
+			'Do not rely on this, it is an API method mostly for developer convenience.',
+			'This does not mean an Upload Campaign editing API will ever be available.',
 		);
 	}
-	
+
 	public function getPossibleErrors() {
 		return array_merge( parent::getPossibleErrors(), array(
 			array( 'missingparam', 'ids' ),
@@ -90,10 +91,10 @@ class ApiDeleteUploadCampaign extends ApiBase {
 			'api.php?action=deleteuploadcampaign&ids=42',
 			'api.php?action=deleteuploadcampaign&ids=4|2',
 		);
-	}	
-	
+	}
+
 	public function getVersion() {
 		return __CLASS__ . ': $Id$';
-	}		
-	
+	}
+
 }
