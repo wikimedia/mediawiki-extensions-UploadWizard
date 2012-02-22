291797ac3881d333d1d0853d9246d058c7875c8f
Sam Reed <reedy@users.mediawiki.org>
2012-02-22 20:56:10 +0000

    Add all index to UploadWizard.sql, then it's easily enableable by using 1 file, rather than having to use 4 files

--- UploadWizard.sql
+++ UploadWizard.sql
@@ -9,9 +9,14 @@ CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/uw_campaigns (
   campaign_enabled         TINYINT             NOT NULL default '0'
 ) /*$wgDBTableOptions*/;
 
+CREATE UNIQUE INDEX /*i*/uw_campaigns_name ON /*_*/uw_campaigns (campaign_name);
+
 -- Upload wizard campaign config
 CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/uw_campaign_conf (
   cc_campaign_id           SMALLINT unsigned   NOT NULL,
   cc_property              VARCHAR(255)        NULL,
   cc_value                 BLOB                NULL
 ) /*$wgDBTableOptions*/;
+
+CREATE UNIQUE INDEX /*i*/uw_cc_id_property ON /*_*/uw_campaign_conf (cc_campaign_id,cc_property);
+CREATE INDEX /*i*/uw_cc_property ON /*_*/uw_campaign_conf (cc_property);
