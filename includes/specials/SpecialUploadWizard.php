3e8b583908876b6df0dc23cc0c7cb7cd0155b774
Chad Horohoe <demon@users.mediawiki.org>
2012-03-21 05:32:14 +0000

    Simplify parameter checking -- 3 local text variables and 7 function calls is also cheaper than 14 function calls

--- includes/specials/SpecialUploadWizard.php
+++ includes/specials/SpecialUploadWizard.php
@@ -53,13 +53,15 @@ class SpecialUploadWizard extends SpecialPage {
 		$this->outputHeader();
 		
 		// if query string includes 'skiptutorial=true' set config variable to true
-		if ( $wgRequest->getCheck( 'skiptutorial' ) ) {
-			$skip = in_array( $wgRequest->getText( 'skiptutorial' ), array( '1', 'true' ) );
+		$skipTutorial = $wgRequest->getCheck( 'skiptutorial' );
+		if ( $skipTutorial ) {
+			$skip = in_array( $skipTutorial, array( '1', 'true' ) );
 			UploadWizardConfig::setUrlSetting( 'skipTutorial', $skip );
 		}
 
-		if ( $wgRequest->getCheck( 'categories' ) ) {
-			UploadWizardConfig::setUrlSetting( 'defaultCategories', explode( '|', $wgRequest->getText( 'categories' ) ) );
+		$categories = $wgRequest->getText( 'categories' );
+		if ( $categories ) {
+			UploadWizardConfig::setUrlSetting( 'defaultCategories', explode( '|', $categories ) );
 		}
 
 		$ulrArgs = array(
@@ -71,8 +73,9 @@ class SpecialUploadWizard extends SpecialPage {
 		);
 
 		foreach ( $ulrArgs as $arg => $setting ) {
-			if ( $wgRequest->getCheck( $arg ) ) {
-				UploadWizardConfig::setUrlSetting( $setting, $wgRequest->getText( $arg ) );
+			$value = $wgRequest->getText( $arg );
+			if ( $value ) {
+				UploadWizardConfig::setUrlSetting( $setting, $value );
 			}
 		}
 
