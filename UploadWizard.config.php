554862a13c8dddf38674fb41ab805fb883ca55d2
YuviPanda <yuvipanda@gmail.com>
2013-06-26 05:58:36 +0530

    Remove random test data
    Change-Id: Id7fc9b59d0cf7ac871dd2baeabdc6dcdbe97ff2e

--- UploadWizard.config.php
+++ UploadWizard.config.php
@@ -147,7 +147,7 @@ return array(
 
 	'defaults' => array(
 		// Categories to list by default in the list of cats to add.
-		'categories' => array( 'helloi' ),
+		'categories' => array( ),
 
 		// Initial value for the description field.
 		'description' => '',
