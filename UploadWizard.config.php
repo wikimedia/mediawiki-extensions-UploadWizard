wikimedia/mediawiki-extensions-UploadWizard

commit 4a530c4b7341b9bb8409b194469f594f24fb5548

Redo campaigns backend to use ContentHandler
Campaigns are now stored as JSON in pages in the Campaign: namespace,
rather than in the database. This lets us treat campaigns the same way
we treat pages - they have history, a talk page, can be deleted /
watchlisted / moved / protected, and can be accessed via the same API
as other pages. Also much easier to extend to add newer fields. As a
downside, no more GUI to create / edit campaigns - but talking to the
primary users of the Campaigns editing interface so far, that does not
seem to be something that is a blocker.

Breaks backwards compatibility with previous versions. Config settings
parameters have changed. Depends on Extension:EventLogging (temporary,
will be removed when JsonSchema + friends move to core). Also depends
on MediaWiki 1.22. Older, mostly unused APIs have been removed. Has a
maintenance script that migrates campaigns from the older, database
format into the newer page format.

(See GitHub link for detailed breakdown of commits to better understand
this huge patch)

GitHub: https://github.com/wikimedia/mediawiki-extensions-UploadWizard/pull/3
Change-Id: Ia77968d909a453b93a55bee4dc8fdc4e075904b9

UploadWizard.config.php
@@ -77,84 +77,87 @@ return array(
 	'flickrApiUrl' => 'http://api.flickr.com/services/rest/?',
 	'flickrApiKey' => '',
 
-	// Categories to automatically (and silently) add all uploaded images into.
-	'autoCategories' => array(),
+	// Settings about things that get automatically (and silently) added to uploads
+	'autoAdd' => array(
+		// Categories to automatically (and silently) add all uploaded images into.
+		'categories' => array(),
 
-	// Deprecated: use autoCategories
-	'autoCategory' => '',
-
-	// Categories to list by default in the list of cats to add.
-	'defaultCategories' => array(),
+		// WikiText to automatically (and silently) add to all uploaded images.
+		'wikitext' => '',
+	),
 
 	// If the user didn't add categories, or removed the default categories, add this wikitext.
 	// Use this to indicate that some human should categorize this file. Does not consider autoCategories, which are hidden.
 	'missingCategoriesWikiText' => '',
 
-	// WikiText to automatically (and silently) add to all uploaded images.
-	'autoWikiText' => '',
+	'display' => array(
+		// Page containing the (wiki)text to display above the UploadWizard UI.
+		// $1 is replaced by the language code.
+		'headerLabelPage' => '',
 
-	// Page containing the (wiki)text to display above the UploadWizard UI.
-	// $1 is replaced by the language code.
-	'headerLabelPage' => '',
+		// Page containing the (wiki)text to display on top of the "use" page.
+		// $1 is replaced by the language code.
+		// When not provided, the message mwe-upwiz-thanks-intro will be used.
+		'thanksLabelPage' => ''
+	),
 
-	// Page containing the (wiki)text to display on top of the "use" page.
-	// $1 is replaced by the language code.
-	// When not provided, the message mwe-upwiz-thanks-intro will be used.
-	'thanksLabelPage' => '',
+	// Settings for the tutorial to be shown.
+	// Empty array if we want to skip
+	'tutorial' => array(
+		// Set to true to skip the tutorial
+		'skip' => false,
 
-	// Default license type.
-	// Possible values: choice, ownwork, thirdparty
-	'defaultLicenseType' => 'choice',
+		// Name of the tutorial on Wikimedia Commons. The $1 is replaced with the language desired.
+		'template' => 'Licensing_tutorial_$1.svg',
 
-	// Should the own work option be shown, and if not, what option should be set?
-	// Possible values: choice, own, notown
-	'ownWorkOption' => 'choice',
+		// The width we want to scale the tutorial to, for our interface.
+		'width' => 720,
 
-	// Name of the tutorial on Wikimedia Commons. The $1 is replaced with the language desired.
-	'tutorialTemplate' => 'Licensing_tutorial_$1.svg',
+		// Imagemap coordinates of the "helpdesk" button at the bottom, which is supposed to be clickable.
+		// Empty string or false to not have an imagemap linked to the helpdesk.
+		'helpdeskCoords' => '27, 1319, 691, 1384',
+	),
 
-	// The width we want to scale the tutorial to, for our interface.
-	'tutorialWidth' => 720,
+	'fields' => array(
+		// Field via which an ID can be provided.
+		array(
+			// When non empty, this field will be shown, and $1 will be replaced by it's value.
+			'wikitext' => '',
 
-	// Imagemap coordinates of the "helpdesk" button at the bottom, which is supposed to be clickable.
-	// Empty string or false to not have an imagemap linked to the helpdesk.
-	'tutorialHelpdeskCoords' => '27, 1319, 691, 1384',
+			// Label text to display with the field.
+			'label' => '',
 
-	// Field via which an ID can be provided.
-	// When non empty, this field will be shown, and $1 will be replaced by it's value.
-	'idField' => '',
+			// Page on which the text to display with the id field is stored.
+			// Overrides label when set. $1 is replaced by the language code.
+			'lagelPage' => '',
 
-	// Label text to display with the id field.
-	'idFieldLabel' => '',
+			// The maximum length of the id field.
+			'maxLength' => 25,
 
-	// Page on which the text to display with the id field is stored.
-	// Overrides idFieldLabel when set. $1 is replaced by the language code.
-	'idFieldLabelPage' => '',
+			// Initial value for the id field.
+			'initialValue' => '',
 
-	// The maximum length of the id field.
-	'idFieldMaxLength' => 25,
+			// Set to true if this field is required
+			'required' => false
+		)
+	),
 
-	// Initial value for the id field.
-	'idFieldInitialValue' => '',
+	'defaults' => array(
+		// Categories to list by default in the list of cats to add.
+		'categories' => array(),
 
-	// Same as above, but for a second field.
-	'idField2' => '',
-	'idField2Label' => '',
-	'idField2LabelPage' => '',
-	'idField2MaxLength' => 25,
-	'idField2InitialValue' => '',
+		// Initial value for the description field.
+		'description' => '',
 
-	// Initial value for the description field.
-	'defaultDescription' => '',
+		// Initial value for the latitude field.
+		'lat' => 0,
 
-	// Initial value for the latitude field.
-	'defaultLat' => '',
+		// Initial value for the longitude field.
+		'lon' => 0,
 
-	// Initial value for the longitude field.
-	'defaultLon' => '',
-
-	// Initial value for the altitude field.
-	'defaultAlt' => '',
+		// Initial value for the altitude field.
+		'alt' => 0,
+	),
 
 	// 'uwLanguages' is a list of languages and codes, for use in the description step.
 	// See the definition of $uwLanguages above. If empty we'll just set a default.
@@ -349,73 +352,81 @@ return array(
 	// their arguments look like strings but they are really templates
 	'licenseTagFilters' => array( 'self' ),
 
-	// radio button selection of some licenses
-	'licensesOwnWork' => array(
-		'type' => 'or',
-		'filterTemplate' => 'self',
-		'licenses' => array(
-			'cc-by-sa-3.0',
-			'cc-by-3.0',
-			'cc-zero'
-		),
-		'defaults' => array( 'cc-by-sa-3.0' )
-	),
+	'licensing' => array(
+		// Default license type.
+		// Possible values: ownwork, thirdparty, choice.
+		'defaultType' => 'choice',
 
-	// checkbox selection of all licenses
-	'licensesThirdParty' => array(
-		'type' => 'or',
-		'licenseGroups' => array(
-			array(
-				// This should be a list of all CC licenses we can reasonably expect to find around the web
-				'head' => 'mwe-upwiz-license-cc-head',
-				'subhead' => 'mwe-upwiz-license-cc-subhead',
-				'licenses' => array(
-					'cc-by-sa-3.0',
-					'cc-by-sa-2.5',
-					'cc-by-3.0',
-					'cc-by-2.5',
-					'cc-zero'
-				)
-			),
-			array(
-				// n.b. as of April 2011, Flickr still uses CC 2.0 licenses.
-				// The White House also has an account there, hence the Public Domain US Government license
-				'head' => 'mwe-upwiz-license-flickr-head',
-				'subhead' => 'mwe-upwiz-license-flickr-subhead',
-				'prependTemplates' => array( 'flickrreview' ),
-				'licenses' => array(
-					'cc-by-sa-2.0',
-					'cc-by-2.0',
-					'pd-usgov',
-				)
-			),
-			array(
-				'head' => 'mwe-upwiz-license-public-domain-usa-head',
-				'subhead' => 'mwe-upwiz-license-public-domain-usa-subhead',
-				'licenses' => array(
-					'pd-us',
-					'pd-art',
-				)
-			),
-			array(
-				// omitted navy because it is believed only MultiChil uses it heavily. Could add it back
-				'head' => 'mwe-upwiz-license-usgov-head',
-				'licenses' => array(
-					'pd-usgov',
-					'pd-usgov-nasa'
-				)
-			),
-			array(
-				'head' => 'mwe-upwiz-license-custom-head',
-				'special' => 'custom',
-				'licenses' => array( 'custom' ),
-			),
-			array(
-				'head' => 'mwe-upwiz-license-none-head',
-				'licenses' => array( 'none' )
-			),
+		// Should the own work option be shown, and if not, what option should be set?
+		// Possible values:  own, notown, choice.
+		'ownWorkDefault' => 'choice',
+
+		// radio button selection of some licenses
+		'ownWork' => array(
+			'type' => 'or',
+			'template' => 'self',
+			'licenses' => array(
+				'cc-by-sa-3.0',
+				'cc-by-3.0',
+				'cc-zero'
+			)
 		),
-		'defaults' => array( 'none' ),
+
+		// checkbox selection of all licenses
+		'thirdParty' => array(
+			'type' => 'or',
+			'licenseGroups' => array(
+				array(
+					// This should be a list of all CC licenses we can reasonably expect to find around the web
+					'head' => 'mwe-upwiz-license-cc-head',
+					'subhead' => 'mwe-upwiz-license-cc-subhead',
+					'licenses' => array(
+						'cc-by-sa-3.0',
+						'cc-by-sa-2.5',
+						'cc-by-3.0',
+						'cc-by-2.5',
+						'cc-zero'
+					)
+				),
+				array(
+					// n.b. as of April 2011, Flickr still uses CC 2.0 licenses.
+					// The White House also has an account there, hence the Public Domain US Government license
+					'head' => 'mwe-upwiz-license-flickr-head',
+					'subhead' => 'mwe-upwiz-license-flickr-subhead',
+					'prependTemplates' => array( 'flickrreview' ),
+					'licenses' => array(
+						'cc-by-sa-2.0',
+						'cc-by-2.0',
+						'pd-usgov',
+					)
+				),
+				array(
+					'head' => 'mwe-upwiz-license-public-domain-usa-head',
+					'subhead' => 'mwe-upwiz-license-public-domain-usa-subhead',
+					'licenses' => array(
+						'pd-us',
+						'pd-art',
+					)
+				),
+				array(
+					// omitted navy because it is believed only MultiChil uses it heavily. Could add it back
+					'head' => 'mwe-upwiz-license-usgov-head',
+					'licenses' => array(
+						'pd-usgov',
+						'pd-usgov-nasa'
+					)
+				),
+				array(
+					'head' => 'mwe-upwiz-license-custom-head',
+					'special' => 'custom',
+					'licenses' => array( 'custom' ),
+				),
+				array(
+					'head' => 'mwe-upwiz-license-none-head',
+					'licenses' => array( 'none' )
+				),
+			)
+		)
 	),
 
 	// Default thumbnail width
@@ -523,9 +534,6 @@ return array(
 		'noUpscaling'       => 'true',
 		'videoCodec'        => 'vp8',
 	),
-
-	// Set skipTutorial to true to always skip tutorial step
-	'skipTutorial' => false,
 
 	// Wiki page for leaving Upload Wizard feedback, for example 'Commons:Upload wizard feedback'
 	'feedbackPage' => '',

UploadWizard.i18n.php
@@ -395,6 +395,10 @@ Please do not use wikitext or HTML here.',
 	'mwe-upwiz-campaign-conf-defaultAlt' => 'Default altitude:',
 	'mwe-upwiz-campaign-conf-defaultDescription' => 'Default description:',
 
+	// Campaign editing edit summaries
+	'mwe-upwiz-campaign-edit-summary-update' => 'Updated campaign configuration via Campaign Editor',
+	'mwe-upwiz-campaign-edit-summary-delete' => 'Deleted campaign via Campaign Editor',
+
 	// Coolcats
 	'mw-coolcats-confirm-new-title' => 'Confirm new category',
 	'mw-coolcats-confirm-new' => 'It looks like you are trying to add a new category, "$1". Be aware:<ul><li>Categories should usually be in English.</li><li>Most new categories should be a subcategory of an existing category.</li></ul>Generally, only experts should create a category.',
@@ -897,6 +901,8 @@ See also:
 	'mwe-upwiz-campaign-conf-defaultLon' => 'Used as label for the longitude field.',
 	'mwe-upwiz-campaign-conf-defaultAlt' => 'Used as label for the altitude field.',
 	'mwe-upwiz-campaign-conf-defaultDescription' => 'Used as label for the description field.',
+	'mwe-upwiz-campaign-edit-summary-update' => 'Edit summary to use when updating campaign configuration',
+	'mwe-upwiz-campaign-edit-summary-delete' => 'Edit summary to use when updating campaign configuration',
 	'mw-coolcats-confirm-new-title' => 'Potwierdź nową kategorię',
 	'mw-coolcats-confirm-new' => 'Unused at this time. Parameters:
 * $1 - category name',

UploadWizard.php
@@ -43,21 +43,22 @@ $wgAutoloadClasses += array(
 	// Hooks
 	'UploadWizardHooks' => $wgUpwizDir . '/UploadWizardHooks.php',
 
-	// Api Modules
-	'ApiUploadCampaign' => $wgUpwizDir . '/api/ApiUploadCampaign.php',
-	'ApiDeleteUploadCampaign' => $wgUpwizDir . '/api/ApiDeleteUploadCampaign.php',
-
 	// Includes
 	'UploadWizardConfig' => $wgUpwizDir . '/includes/UploadWizardConfig.php',
 	'UploadWizardTutorial' => $wgUpwizDir . '/includes/UploadWizardTutorial.php',
 	'UploadWizardCampaign' => $wgUpwizDir . '/includes/UploadWizardCampaign.php',
-	'UploadWizardCampaigns' => $wgUpwizDir . '/includes/UploadWizardCampaigns.php',
+
+	// Campaign ContentHandler
+	'CampaignContentHandler' => $wgUpwizDir . '/includes/CampaignContentHandler.php',
+	'CampaignContent' => $wgUpwizDir . '/includes/CampaignContent.php',
+	'CampaignHooks' => $wgUpwizDir . '/includes/CampaignHooks.php',
 
 	// Special Pages
 	'SpecialUploadWizard' => $wgUpwizDir . '/includes/specials/SpecialUploadWizard.php',
-	'SpecialUploadCampaigns' => $wgUpwizDir . '/includes/specials/SpecialUploadCampaigns.php',
-	'SpecialUploadCampaign' => $wgUpwizDir . '/includes/specials/SpecialUploadCampaign.php'
 );
+
+// Registers hook and content handlers for Campaign contenthandler
+$wgExtensionFunctions[] = 'CampaignHooks::registerHandlers';
 
 // $wgAPIModules['titlecheck'] = 'ApiTitleCheck';
 // $wgAPIListModules['titlecheck'] = 'ApiTitleCheck';
@@ -65,20 +66,6 @@ $wgAutoloadClasses += array(
 # Let the special page be a special center of unique specialness
 $wgSpecialPages['UploadWizard'] = 'SpecialUploadWizard';
 $wgSpecialPageGroups['UploadWizard'] = 'media';
-
-$wgSpecialPages['UploadCampaigns'] = 'SpecialUploadCampaigns';
-$wgSpecialPageGroups['UploadCampaigns'] = 'media';
-
-$wgSpecialPages['UploadCampaign'] = 'SpecialUploadCampaign';
-$wgSpecialPageGroups['UploadCampaign'] = 'media';
-
-$wgAPIModules['uploadcampaign'] = 'ApiUploadCampaign';
-$wgAPIModules['deleteuploadcampaign'] = 'ApiDeleteUploadCampaign';
-
-if ( array_key_exists( 'ApiQueryORM', $wgAutoloadLocalClasses ) ) { // Backwards-compatibility with MW 1.20
-	$wgAutoloadClasses['ApiQueryUploadCampaigns'] = $wgUpwizDir . '/api/ApiQueryUploadCampaigns.php';
-	$wgAPIListModules['uploadcampaigns'] = 'ApiQueryUploadCampaigns';
-}
 
 // for ResourceLoader
 $wgHooks['ResourceLoaderRegisterModules'][] = 'UploadWizardHooks::resourceLoaderRegisterModules';
@@ -108,3 +95,8 @@ $wgDefaultUserOptions['upwiz_maxsimultaneous'] = 'default';
 // UploadWizard.config.php includes default configuration
 // any value can be modified in localSettings.php by setting  $wgUploadWizardConfig['name'] = 'value;
 $wgUploadWizardConfig = array();
+
+/* Define default namespaces, as defined on Mediawiki.org
+ * https://www.mediawiki.org/wiki/Extension_default_namespaces#UploadWizard */
+define( 'NS_CAMPAIGN', 460 );
+define( 'NS_CAMPAIGN_TALK', 461 );

UploadWizard.sql
@@ -3,6 +3,9 @@
 -- Author: Jeroen De Dauw < jeroendedauw@gmail.com >
 
 -- Upload wizard campaigns
+-- This is *not* the primary storage for campaigns.
+-- Just stores a copy of information that is already present in the
+-- appropriate wikipages, for easier indexing / querying
 CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/uw_campaigns (
   campaign_id              INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
   campaign_name            VARCHAR(255)        NOT NULL,
@@ -10,13 +13,3 @@ CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/uw_campaigns (
 ) /*$wgDBTableOptions*/;
 
 CREATE UNIQUE INDEX /*i*/uw_campaigns_name ON /*_*/uw_campaigns (campaign_name);
-
--- Upload wizard campaign config
-CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/uw_campaign_conf (
-  cc_campaign_id           SMALLINT unsigned   NOT NULL,
-  cc_property              VARCHAR(255)        NULL,
-  cc_value                 BLOB                NULL
-) /*$wgDBTableOptions*/;
-
-CREATE UNIQUE INDEX /*i*/uw_cc_id_property ON /*_*/uw_campaign_conf (cc_campaign_id,cc_property);
-CREATE INDEX /*i*/uw_cc_property ON /*_*/uw_campaign_conf (cc_property);

UploadWizardHooks.php
@@ -535,20 +535,6 @@ class UploadWizardHooks {
 			dirname( __FILE__ ) . '/sql/UW_IndexCampaignsName.sql',
 			true
 		) );
-		$updater->addExtensionUpdate( array(
-			'addIndex',
-			'uw_campaign_conf',
-			'uw_cc_id_property',
-			dirname( __FILE__ ) . '/sql/UW_IndexConfIdProp.sql',
-			true
-		) );
-		$updater->addExtensionUpdate( array(
-			'addIndex',
-			'uw_campaign_conf',
-			'uw_cc_property',
-			dirname( __FILE__ ) . '/sql/UW_IndexConfProp.sql',
-			true
-		) );
 
 		return true;
 	}
@@ -569,7 +555,7 @@ class UploadWizardHooks {
 		$config = UploadWizardConfig::getConfig();
 
 		// User preference to skip the licensing tutorial, provided it's not globally disabled
-		if ( UploadWizardConfig::getSetting( 'skipTutorial' ) == false ) {
+		if ( UploadWizardConfig::getSetting( 'tutorial' ) != array() ) {
 			$preferences['upwiz_skiptutorial'] = array(
 				'type' => 'check',
 				'label-message' => 'mwe-upwiz-prefs-skiptutorial',
@@ -582,7 +568,9 @@ class UploadWizardHooks {
 
 			$licenses = array();
 
-			$ownWork = UploadWizardConfig::getSetting( 'licensesOwnWork' );
+			$licensingOptions = UploadWizardConfig::getSetting( 'licensing' );
+
+			$ownWork = $licensingOptions['ownWork'];
 			foreach ( $ownWork['licenses'] as $license ) {
 				$licenseMessage = self::getLicenseMessage( $license, $licenseConfig );
 				$licenses[wfMessage( 'mwe-upwiz-prefs-license-own', $licenseMessage )->text()] = 'ownwork-' . $license;

api/ApiDeleteUploadCampaign.php
@@ -1,89 +1,0 @@
-<?php
-
-/**
- * API module to delete upload wizard campaigns.
- *
- * @since 1.2
- *
- * @file ApiDeleteUploadCampaign.php
- * @ingroup Upload
- * @ingroup API
- *
- * @licence GNU GPL v3+
- * @author Jeroen De Dauw < jeroendedauw@gmail.com >
- */
-class ApiDeleteUploadCampaign extends ApiBase {
-
-	public function execute() {
-		$user = $this->getUser();
-
-		if ( !$user->isAllowed( 'upwizcampaigns' ) || $user->isBlocked() ) {
-			$this->dieUsageMsg( array( 'badaccess-groups' ) );
-		}
-
-		$params = $this->extractRequestParams();
-
-		$this->getResult()->addValue(
-			null,
-			'success',
-			UploadWizardCampaigns::singleton()->delete( array( 'id' => $params['ids'] ), __METHOD__ )
-		);
-	}
-
-	public function needsToken() {
-		return true;
-	}
-
-	public function getTokenSalt() {
-		$params = $this->extractRequestParams();
-		return 'deletecampaign' . implode( '|', $params['ids'] );
-	}
-
-	public function mustBePosted() {
-		return true;
-	}
-
-	public function getAllowedParams() {
-		return array(
-			'ids' => array(
-				ApiBase::PARAM_TYPE => 'integer',
-				ApiBase::PARAM_REQUIRED => true,
-				ApiBase::PARAM_ISMULTI => true,
-			),
-			'token' => null,
-		);
-	}
-
-	public function getParamDescription() {
-		return array(
-			'ids' => 'The IDs of the campaigns to delete',
-			'token' => 'Edit token. You can get one of these through prop=info.',
-		);
-	}
-
-	public function getDescription() {
-		return array(
-			'API module for deleting Upload Campaigns, associated with UploadWizard.',
-			'Do not rely on this, it is an API method mostly for developer convenience.',
-			'This does not mean an Upload Campaign editing API will ever be available.',
-		);
-	}
-
-	public function getPossibleErrors() {
-		return array_merge( parent::getPossibleErrors(), array(
-			array( 'missingparam', 'ids' ),
-		) );
-	}
-
-	protected function getExamples() {
-		return array(
-			'api.php?action=deleteuploadcampaign&ids=42',
-			'api.php?action=deleteuploadcampaign&ids=4|2',
-		);
-	}
-
-	public function getVersion() {
-		return __CLASS__ . ': 1.0';
-	}
-
-}

api/ApiQueryUploadCampaigns.php
@@ -1,214 +1,0 @@
-<?php
-
-/**
- * API module to query upload campaign information.
- *
- * @since 1.3
- *
- * @file
- * @ingroup UploadWizard
- * @ingroup API
- *
- * @licence GNU GPL v2+
- * @author Jeroen De Dauw < jeroendedauw@gmail.com >
- * @author Brion Vibber <brion@wikimedia.org>
- */
-class ApiQueryUploadCampaigns extends ApiQueryORM {
-
-	/**
-	 * @see ApiQueryBase::__construct()
-	 * @param ApiBase $main
-	 * @param string $action
-	 */
-	public function __construct( $main, $action ) {
-		parent::__construct( $main, $action, 'uc' );
-	}
-
-	/**
-	 * @see ApiQueryORM::getTable()
-	 * @return IORMTable
-	 */
-	protected function getTable() {
-		return UploadWizardCampaigns::singleton();
-	}
-
-	/**
-	 * @see ApiQueryORM::getRowName()
-	 * @return string
-	 */
-	protected function getRowName() {
-		return 'campaign';
-	}
-
-	/**
-	 * @see ApiQueryORM::getRowName()
-	 * @return string
-	 */
-	protected function getListName() {
-		return 'campaigns';
-	}
-
-	/**
-	 * @see ApiQueryORM::getResultPath()
-	 * @return string
-	 */
-	protected function getResultPath() {
-		return 'uploadcampaign';
-	}
-
-	/**
-	 * @see ApiQueryORM::formatRow()
-	 * @param array $params
-	 * @param array $conditions
-	 * @return ORMResult
-	 */
-	protected function getResults( array $params, array $conditions ) {
-		$configNames = array();
-
-		if ( in_array( 'config', $params['props'] ) ) {
-			$configNames = array_key_exists( 'config', $params ) ? $params['config'] : null;
-		}
-
-		$props = array_filter( $params['props'], function( $prop ) { return $prop !== 'config'; } );
-
-		return $this->getTable()->batchSelect(
-			$props,
-			$conditions,
-			$configNames,
-			array(
-				'LIMIT' => $params['limit'] + 1,
-				'ORDER BY' => $this->getTable()->getPrefixedField( 'id' ) . ' ASC'
-			)
-		);
-	}
-
-	/**
-	 * @see ApiQueryORM::formatRow()
-	 * @param IORMRow $campaign
-	 * @param array $params
-	 * @return mixed
-	 */
-	protected function formatRow( IORMRow $campaign, array $params ) {
-		$item = array();
-
-		if ( $campaign->hasField( 'name' ) ) {
-			$item['name'] = $this->safeElement( 'name', $campaign->getName() );
-		}
-
-		if ( $campaign->hasField( 'id' ) ) {
-			$item['id'] = $this->safeElement( 'id', $campaign->getId() );
-		}
-
-		if ( $campaign->hasField( 'enabled' ) ) {
-			$item['isenabled'] = $this->safeElement( 'isenabled', intval( $campaign->getIsEnabled() ) );
-		}
-
-		if ( in_array( 'config', $params['props'] ) ) {
-			$config = $campaign->getConfig();
-			$item['config'] = array();
-
-			foreach ( $config as $key => $val ) {
-				if ( is_array( $val ) ) {
-					$keys = array(
-						'licensesOwnWork' => 'license',
-						'defaultCategories' => 'category',
-						'autoCategories' => 'category',
-					);
-					$this->getResult()->setIndexedTagName( $val, $keys[$key], $key );
-				} else {
-					$val = $this->safeElement( $key, $val );
-				}
-
-				$item['config'][$key] = $val;
-			}
-		}
-
-		return $item;
-	}
-
-	/**
-	 * Wrap something in an element for XML, or nice plain JSON
-	 */
-	protected function safeElement( $key, $val ) {
-		if ( $this->getResult()->getIsRawMode() ) {
-			return array(
-				'_element' => $key,
-				'*' => $val
-			);
-		} else {
-			return $val;
-		}
-	}
-
-	/**
-	 * @see ApiBase::getDescription()
-	 * @return string
-	 */
-	public function getDescription() {
-		return 'API module for querying upload campaigns';
-	}
-
-	/**
-	 * @see ApiBase::getParamDescription()
-	 * @return array
-	 */
-	public function getParamDescription() {
-		return array(
-			'name' => 'The name(s) of the campaign(s) to fetch info on.',
-			'id' => 'The id(s) of the campaigns(s) to fetch info on.',
-			'enabled' => 'If set, only campaigns that are (not) enabled will be returned.',
-			'config' => 'The names of the configuration values to obtain. If not set all values will be returned.'
-		);
-	}
-
-	/**
-	 * @see ApiBase::getAllowedParams()
-	 * @return array
-	 */
-	public function getAllowedParams() {
-		$params = array_merge(
-			parent::getAllowedParams(),
-			array(
-				'config' => array(
-					ApiBase::PARAM_TYPE => 'string',
-					ApiBase::PARAM_ISMULTI => true,
-				),
-			)
-		);
-
-		$params['props'][ApiBase::PARAM_TYPE][] = 'config';
-
-		$params['name'][ApiBase::PARAM_ISMULTI] = true;
-		$params['id'][ApiBase::PARAM_ISMULTI] = true;
-
-		return $params;
-	}
-
-	/**
-	 * @see ApiBase::getExamples()
-	 * @return array
-	 */
-	protected function getExamples() {
-		return array (
-			'api.php?action=query&list=uploadcampaigns&ucname=wlm-nl&ucprop=config'
-			=> 'Get the info and all configuration of the wlm-nl campaign.',
-			'api.php?action=query&list=uploadcampaigns&ucname=wlm-nl|wlm-de&ucprop=config'
-			=> 'Get the info and all configuration of the wlm-nl and wlm-de campaigns.',
-			'api.php?action=query&list=uploadcampaigns&ucid=42'
-			=> 'Get the info of the campaign with id 42.',
-			'api.php?action=query&list=uploadcampaigns'
-			=> 'Get the info of all the campaigns',
-			'api.php?action=query&list=uploadcampaigns&ucenabled=1'
-			=> 'Get the info of all the enabled campaigns',
-		);
-	}
-
-	/**
-	 * @see ApiBase::getVersion()
-	 * @return string
-	 */
-	public function getVersion() {
-		return __CLASS__ . ': 1.0';
-	}
-
-}

commit 75f9a2a5a571122b572821a8a624b74d0039bed9

Do not set default values for lat and lon
Otherwise a location template is added with |0|0 coordinates (yeah! they
are valid) to each uploaded file if the user did not clear the input
fields that are hidden by default.

Can't set them to empty string, due to limtations in JsonSchema.
Setting them to null or empty string causes problems when editing
Campaign: pages

Regression introduced in 4a530c4b7341b9bb8409b194469f594f24fb5548

Bug: 51879
Change-Id: I1c3ddca19f36a7910f587a66f95b53d9facf4449

UploadWizard.config.php
@@ -143,14 +143,19 @@ return array(
 		// Initial value for the description field.
 		'description' => '',
 
+		// These values are commented out by default, so they can be undefined
+		// Define them here if you want defaults.
+		// This is required, because the JsonSchema for these defines them to be type number
+		// But we can't have them to be NULL, because that's not a number.
+		// This is a technical limitation of JsonSchema, I think.
 		// Initial value for the latitude field.
-		'lat' => 0,
+		//'lat' => 0,
 
-		// Initial value for the longitude field.
-		'lon' => 0,
+		//// Initial value for the longitude field.
+		//'lon' => 0,
 
-		// Initial value for the altitude field.
-		'alt' => 0,
+		//// Initial value for the altitude field.
+		//'alt' => 0,
 	),
 
 	// 'uwLanguages' is a list of languages and codes, for use in the description step.