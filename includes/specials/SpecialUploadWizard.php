aff45b9cfac7c906aa66ec60298913c14c502ff5
YuviPanda <yuvipanda@gmail.com>
2013-07-30 17:13:30 +0530

    Add separate config parameter for tracking categories
    Bug: 51929
    Change-Id: Id1cb786b46c9cab609403fe07667b0315792b83d

--- includes/specials/SpecialUploadWizard.php
+++ includes/specials/SpecialUploadWizard.php
@@ -188,6 +188,17 @@ class SpecialUploadWizard extends SpecialPage {
 			}
 		}
 
+		if ( array_key_exists( 'trackingCategory', $config )  ) {
+			if ( $this->campaign !== null && array_key_exists( 'campaign', $config['trackingCategory'] ) ) {
+				$config['trackingCategory']['campaign'] = str_replace(
+					'$1',
+					$this->campaign,
+					$config['trackingCategory']['campaign']
+				);
+			} else {
+				unset ( $config['trackingCategory']['campaign'] );
+			}
+		}
 		// UploadFromUrl parameter set to true only if the user is allowed to upload a file from a URL which we need to check in our Javascript implementation.
 		if ( UploadFromUrl::isEnabled() && UploadFromUrl::isAllowed( $this->getUser() ) === true ) {
 			$config['UploadFromUrl'] = true;
