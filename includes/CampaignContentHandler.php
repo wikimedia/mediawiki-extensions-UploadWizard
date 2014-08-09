b7dfa860add52f515b444549116c2f8eb3040853
Kunal Mehta <legoktm@gmail.com>
2014-08-09 12:05:58 +0100

    Convert Campaigns to extend JSONContent from core
    Change-Id: I5de25bfa84e490974a0106730fa0252645a647f7

--- includes/CampaignContentHandler.php
+++ includes/CampaignContentHandler.php
@@ -10,41 +10,11 @@
  * @author Yuvi Panda <yuvipanda@gmail.com>
  */
 
-class CampaignContentHandler extends TextContentHandler {
+class CampaignContentHandler extends JSONContentHandler {
 
 	public function __construct( $modelId = 'Campaign' ) {
 		parent::__construct( $modelId, array( CONTENT_FORMAT_JSON ) );
 	}
 
-	/**
-	 * Unserializes a CampaignContent object.
-	 *
-	 * @param string $text Serialized form of the content
-	 * @param null|string $format The format used for serialization
-	 *
-	 * @return Content the CampaignContent object wrapping $text
-	 */
-	public function unserializeContent( $text, $format = null ) {
-		$this->checkFormat( $format );
-		return new CampaignContent( $text );
-	}
-
-	/**
-	 * Creates an empty CampaignContent object.
-	 *
-	 * @return Content
-	 */
-	public function makeEmptyContent() {
-		return new CampaignContent( '' );
-	}
-
-	/** JSON Schema is English **/
-	public function getPageLanguage( Title $title, Content $content = null ) {
-		return wfGetLangObj( 'en' );
-	}
-
-	/** JSON Schema is English **/
-	public function getPageViewLanguage( Title $title, Content $content = null ) {
-		return wfGetLangObj( 'en' );
-	}
+	protected $contentClass = 'CampaignContent';
 }
