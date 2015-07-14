97cf22f2d6249f00268d08bb9206bb255e6a78bd
zhuyifei1999 <zhuyifei1999@gmail.com>
2015-07-14 11:06:03 +0000

    Flickr: Added support for cc-zero & public domain mark
    * cc-zero images are handled like other images with the license tag of "{{FlickrVerifiedByUploadWizard|cc-zero}}{{cc-zero}}"
    * public domain mark images are handled with:
    ** set "{{FlickrVerifiedByUploadWizard|Public Domain Mark}}" as the prefix for the license tag
    ** set the license boolean parameter to false to indicate that the exact license tag is uncertain
    *** The boolean value was originally added in Ice8d3a998f862d1fb959a5f8b0f9e0046d0f99fe
    *** the value was always true, and false seemed to be meaningless
    ** when the value is false, a textbox input is added for the custom pd license tag
    *** The default configuration does not allow the textbox to be blank
    ** Final output is something like (before substitution):
    **: "{{FlickrVerifiedByUploadWizard|Public Domain Mark}}{{subst:Custom license marker added by UW}}\n{{PD-blah}}"
    
    Bug: T105629
    Change-Id: If6e402fccc8173d19a88677d7167e734adec7425

--- resources/mw.FlickrChecker.js
+++ resources/mw.FlickrChecker.js
@@ -49,7 +49,9 @@
 			'Attribution-NonCommercial-ShareAlike License': 'invalid',
 			'Attribution-ShareAlike License': '{{FlickrVerifiedByUploadWizard|cc-by-sa-2.0}}{{cc-by-sa-2.0}}',
 			'No known copyright restrictions': '{{FlickrVerifiedByUploadWizard|Flickr-no known copyright restrictions}}{{Flickr-no known copyright restrictions}}',
-			'United States Government Work': '{{FlickrVerifiedByUploadWizard|PD-USGov}}{{PD-USGov}}'
+			'United States Government Work': '{{FlickrVerifiedByUploadWizard|PD-USGov}}{{PD-USGov}}',
+			'Public Domain Dedication (CC0)': '{{FlickrVerifiedByUploadWizard|cc-zero}}{{cc-zero}}',
+			'Public Domain Mark': '{{FlickrVerifiedByUploadWizard|Public Domain Mark}}' // T105629
 		},
 
 		/**
@@ -372,7 +374,7 @@
 						fromURL: true,
 						licenseValue: licenseValue,
 						licenseMessage: license.licenseMessage,
-						license: true,
+						license: license.licenseName !== 'Public Domain Mark',
 						photoId: item.id,
 						location: {
 							latitude:item.latitude,
@@ -478,7 +480,7 @@
 					fromURL: true,
 					licenseValue: license.licenseValue,
 					licenseMessage: license.licenseMessage,
-					license: true,
+					license: license.licenseName !== 'Public Domain Mark',
 					author: photoAuthor,
 					originalFormat: photo.originalformat,
 					date: photo.dates.taken,
