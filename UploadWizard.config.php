0c142e5d6c520d45877a822da6d67499396db428
PranavK <pranavmk98@gmail.com>
2015-12-28 07:26:32 +0530

    Define required 'defaults' field in licensing config
    Bug: T65494
    Change-Id: I96b041e3cf4ba2f1f5cfd3dc96fa677280c36d9b

--- UploadWizard.config.php
+++ UploadWizard.config.php
@@ -434,6 +434,13 @@ return array(
 		'ownWork' => array(
 			'type' => 'or',
 			'template' => 'self',
+			'defaults' => array(
+				'cc-by-sa-4.0',
+				'cc-by-sa-3.0',
+				'cc-by-4.0',
+				'cc-by-3.0',
+				'cc-zero'
+			),
 			'licenses' => array(
 				'cc-by-sa-4.0',
 				'cc-by-sa-3.0',
@@ -446,6 +453,13 @@ return array(
 		// checkbox selection of all licenses
 		'thirdParty' => array(
 			'type' => 'or',
+			'defaults' => array(
+				'cc-by-sa-4.0',
+				'cc-by-sa-3.0',
+				'cc-by-4.0',
+				'cc-by-3.0',
+				'cc-zero'
+			),
 			'licenseGroups' => array(
 				array(
 					// This should be a list of all CC licenses we can reasonably expect to find around the web
