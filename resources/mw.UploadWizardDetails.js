d9604e759a3d8a8d39d13511ee12e9f450049bf8
Bartosz Dziewoński <matma.rex@gmail.com>
2016-06-28 00:09:28 +0200

    mw.UploadWizardDetails: Fix handling for 'titleblacklist-forbidden' error
    Follow-up to 4a62235c084672af3573f134480d58269cb3a092 and
    ecd2b9ce4ea9abb3611cdd534c71c7c2107ffc30. This almost never
    actually runs, as we detect TitleBlacklist issues before
    allowing the user to submit.
    
    Change-Id: I17dd233d7dd42b961d34627ebc49e82154dc765f

--- resources/mw.UploadWizardDetails.js
+++ resources/mw.UploadWizardDetails.js
@@ -988,7 +988,7 @@
 					this.recoverFromError( mw.message( 'mwe-upwiz-error-title-' + titleErrorMap[ code ] ), 'title-' + titleErrorMap[ code ] );
 					return;
 				} else if ( code === 'titleblacklist-forbidden' ) {
-					this.recoverFromError( this.titleId, mw.message( 'mwe-upwiz-error-title-' + titleBlacklistMessageMap[ result.error.message ] ), 'title-' + titleBlacklistMessageMap[ result.error.message ] );
+					this.recoverFromError( mw.message( 'mwe-upwiz-error-title-' + titleBlacklistMessageMap[ result.error.message ] ), 'title-' + titleBlacklistMessageMap[ result.error.message ] );
 					return;
 				} else {
 					statusKey = 'api-error-' + code;
