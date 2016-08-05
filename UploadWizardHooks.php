69dafe72c754c2f94f2ae304e39b31b7f6d02266
Prateek Saxena <prtksxna@gmail.com>
2016-08-05 12:57:00 +0530

    Remove 'UnitTestList' hook
    No longer needed now that extension unittests are autodiscovered.
    
    Bug: T142120
    Change-Id: I0481df1ba26d39ef75cfc9a7d8c1a559ee295893

--- UploadWizardHooks.php
+++ UploadWizardHooks.php
@@ -184,17 +184,6 @@ class UploadWizardHooks {
 	}
 
 	/**
-	 * Hook to add unit tests
-	 * @param array $files list of testcases
-	 * @return bool
-	 */
-	public static function onUnitTestsList( array &$files ) {
-		$testDir = __DIR__ . DIRECTORY_SEPARATOR . 'tests' . DIRECTORY_SEPARATOR . 'phpunit';
-		$files = array_merge( $files, glob( $testDir . DIRECTORY_SEPARATOR . '*Test.php' ) );
-		return true;
-	}
-
-	/**
 	 * Helper function to get the message for a license.
 	 *
 	 * @since 1.2
