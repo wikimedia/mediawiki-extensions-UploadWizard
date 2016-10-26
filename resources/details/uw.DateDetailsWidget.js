6b1f0d337472e8a4032017867c4658d5e72fd501
Bartosz Dziewoński <matma.rex@gmail.com>
2016-10-27 01:20:32 +0200

    Unbreak Flickr uploads
    deedChooser might not be set for Flickr uploads at all.
    
    Bug: T149259
    Change-Id: If83d0b176155e87d5aee5ebe294bb76f4d08ac2d

--- resources/details/uw.DateDetailsWidget.js
+++ resources/details/uw.DateDetailsWidget.js
@@ -103,7 +103,7 @@
 	 * @return {Object}
 	 */
 	uw.DateDetailsWidget.prototype.getLicenses = function () {
-		if ( this.upload.deedChooser.deed && this.upload.deedChooser.deed.licenseInput ) {
+		if ( this.upload.deedChooser && this.upload.deedChooser.deed && this.upload.deedChooser.deed.licenseInput ) {
 			return this.upload.deedChooser.deed.licenseInput.getLicenses();
 		}
 
