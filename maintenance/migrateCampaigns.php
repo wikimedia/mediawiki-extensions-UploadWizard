4e875119cd704f5fb13d43bf4104cc22d154b41d
zoranzoki21 <zorandori4444@gmail.com>
2018-01-28 20:21:13 +0100

    Update Maintenance scripts in extensions to use $this->requireExtension()
    Bug: T152139
    Change-Id: Ib8b244f0d1b5aa3d06dfb522da6abcfd31ca67db

--- maintenance/migrateCampaigns.php
+++ maintenance/migrateCampaigns.php
@@ -47,6 +47,8 @@ class MigrateCampaigns extends Maintenance {
 
 	public function __construct() {
 		parent::__construct();
+
+		$this->requireExtension( 'Upload Wizard' );
 		$this->mDescription = "Migrate UploadCampaigns from database storage to pages";
 		$this->addOption( 'user', 'The user to perform the migration as', false, true, 'u' );
 	}
