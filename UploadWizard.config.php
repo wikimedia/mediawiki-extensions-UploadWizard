2be72613746c7b3e9bf04d3948634ca006e40f8c
Reedy <reedy@wikimedia.org>
2018-07-17 21:36:09 +0100

    Replace foundationwiki url
    Bug: T199812
    Change-Id: If8be70f78a393ed2cc7b26f3959e325862705ee1

--- UploadWizard.config.php
+++ UploadWizard.config.php
@@ -535,7 +535,7 @@ return [
 		'extensions' => [ 'stl' ],
 		'template' => '3dpatent',
 		'url' => [
-			'legalcode' => '//wikimediafoundation.org/wiki/Wikimedia_3D_file_patent_license',
+			'legalcode' => '//foundation.wikimedia.org/wiki/Wikimedia_3D_file_patent_license',
 			'warranty' => '//meta.wikimedia.org/wiki/Wikilegal/3D_files_and_3D_printing',
 			'license' => '//meta.wikimedia.org/wiki/Wikilegal/3D_files_and_3D_printing',
 			'weapons' => '//meta.wikimedia.org/wiki/Wikilegal/3D_files_and_3D_printing#Weapons',
