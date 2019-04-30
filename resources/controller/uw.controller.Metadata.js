19b2995a9f0c60850c6fef500dd5c87dfda2fd74
Matthias Mullie <git@mullie.eu>
2019-04-30 12:16:09 +0200

    Show uploads in the same order as in other steps
    Because we also need to fetch the entity ids for the newly
    submitted files, there's no guarantee on the order in which
    the statements widgets are created. So:
    - we need to make sure they are added in the correct order,
      by index
    - we shouldn't add things until the first one is ready: the
      first one to be added will automatically be selected, and
      we want that one to be the actual first upload
    - since we could be waiting a little while until the first
      item has been added, let's display a spinner to indicate
      something is happening
    
    And while I shifted some things around, might as well
    re-use the array of promises in the onSubmit method, instead
    of keeping track of the statements widgets in yet another var.
    
    Bug: T221077
    Change-Id: I4a2f0e4fb965fdaac805fda6bff021d3383b863c

--- resources/controller/uw.controller.Metadata.js
+++ resources/controller/uw.controller.Metadata.js
@@ -54,47 +54,65 @@
 
 		uw.controller.Step.prototype.load.call( this, uploads );
 
-		this.statements = {};
-		uploads.forEach( function ( upload ) {
-			upload.details.getMediaInfoEntityId().then( function ( entityId ) {
-				var statements = [],
-					content, page;
+		// show a spinner while the statement widgets are being created
+		// (this could take a little time as it might involve API calls
+		// to figure out the new pages' relevant entityIDs)
+		this.ui.renderContent( $.createSpinner( { size: 'large', type: 'block' } ) );
 
-				Object.keys( mw.config.get( 'wbmiProperties' ) ).forEach( function ( propertyId ) {
-					var statement = new mw.mediaInfo.statements.StatementWidget( {
-						entityId: entityId,
-						propertyId: propertyId
-					} );
-					statements.push( statement );
+		this.statementPromises = uploads.map( this.getStatementWidgetsForUpload );
+
+		// don't render the booklet until the first page is ready
+		this.statementPromises[ 0 ].then( function () {
+			self.statementPromises.forEach( function ( statementPromise, i ) {
+				statementPromise.then( function ( statements ) {
+					var upload = uploads[ i ],
+						content = new uw.MetadataContent( upload, statements ),
+						page = new uw.MetadataPage( upload, { expanded: false, content: [ content ] } );
+
+					booklet.addPages( [ page ], i );
 				} );
-
-				content = new uw.MetadataContent( upload, statements );
-				page = new uw.MetadataPage( upload, { expanded: false, content: [ content ] } );
-
-				self.statements[ entityId ] = statements;
-				booklet.addPages( [ page ] );
 			} );
-		} );
 
-		this.ui.renderContent( booklet.$element );
+			booklet.selectFirstSelectablePage();
+			self.ui.renderContent( booklet.$element );
+		} );
+	};
+
+	/**
+	 * @param {mw.UploadWizardUpload} upload
+	 * @return {jQuery.Promise}
+	 */
+	uw.controller.Metadata.prototype.getStatementWidgetsForUpload = function ( upload ) {
+		return upload.details.getMediaInfoEntityId().then( function ( entityId ) {
+			var statements = [];
+
+			Object.keys( mw.config.get( 'wbmiProperties' ) ).forEach( function ( propertyId ) {
+				var statement = new mw.mediaInfo.statements.StatementWidget( {
+					entityId: entityId,
+					propertyId: propertyId
+				} );
+				statements.push( statement );
+			} );
+
+			return statements;
+		} );
 	};
 
 	uw.controller.Metadata.prototype.onSubmit = function () {
-		var self = this;
+		return $.when.apply( $, this.statementPromises ).then( function () {
+			return $.when.apply( $, [].slice.call( arguments ).map( function ( statements ) {
+				var promise = $.Deferred().resolve().promise();
 
-		$.when.apply( $, Object.keys( this.statements ).map( function ( entityId ) {
-			// we can start submitting statements for multiple files at the time
-			// time, but multiple statements per entity need to be submitted sequentially
-			// (to avoid them being considered edit conflicts)
-			var promise = $.Deferred().resolve().promise(),
-				statements = self.statements[ entityId ];
+				// we can start submitting statements for multiple files at the time
+				// time, but multiple statements per entity need to be submitted sequentially
+				// (to avoid them being considered edit conflicts)
+				statements.forEach( function ( statement ) {
+					promise = promise.then( statement.submit.bind( statement ) );
+				} );
 
-			statements.forEach( function ( statement ) {
-				promise = promise.then( statement.submit.bind( statement ) );
-			} );
-
-			return promise;
-		} ) ).then( this.moveNext.bind( this ) );
+				return promise;
+			} ) );
+		} ).then( this.moveNext.bind( this ) );
 	};
 
 }( mw.uploadWizard ) );
