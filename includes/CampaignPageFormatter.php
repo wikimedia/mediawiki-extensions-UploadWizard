86da859f8ea422361512de8e894070b6cb7d4e0a
Ahmon Dancy <adancy@wikimedia.org>
2024-09-05 21:16:25 +0000

    Revert "Set parser for image gallery in CampaignPageFormatter"
    This reverts commit fdbc13f12c04c0b900dd50679009af87647c0e74.
    
    Reason for revert: https://phabricator.wikimedia.org/T374146#10123311
    
    Change-Id: I1bb2260a83da751dec0fbe1b78391a22bde5b49a

--- includes/CampaignPageFormatter.php
+++ includes/CampaignPageFormatter.php
@@ -55,7 +55,6 @@ class CampaignPageFormatter {
 
 		$gallery = ImageGalleryBase::factory( 'packed-hover' );
 		$gallery->setContext( $this->context );
-		$gallery->setParser( MediaWikiServices::getInstance()->getParserFactory()->create() );
 		$gallery->setWidths( '180' );
 		$gallery->setHeights( '180' );
 		$gallery->setShowBytes( false );
